caf9e77b60a47de000cfa3bc356e408fd49b1741
jordanoboyle <jmcmcode@gmail.com>
2024-09-18 22:59:58 -0600

    consts established for the JS portion

---
+++ script.js
@@ -0,0 +1,9 @@
+//This is new to me. 
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  singleMealEl = document.getElementById('single-meal');
+
+
