f1342d5f5c333ee66bf78812ac1f9efbd5d1dbb3
jordanoboyle <jmcmcode@gmail.com>
2024-09-18 23:45:44 -0600

    added the fetch dynamic search'

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const search = document.getElementById('search'),
   singleMealEl = document.getElementById('single-meal');
 
 
+//Search for Meal and Fetch
+function searchMeals(e) {
+  e.preventDefault();
+
+  //Clear Single Meal
+  singleMealEl.innerHTML = '';
+
+  //Get Search Term
+
+  const term = search.value;
+
+  //Check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  } else {
+    alert('Please Enter a Search Term'); // This is a simple JS way to handle a faulty input, thought it is better to update a DOM element that will tell you otherwise
+  }
+
+}
+
+//Event Listeners 
+submit.addEventListener('submit', searchMeals);
